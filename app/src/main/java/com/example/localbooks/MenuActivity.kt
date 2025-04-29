bcc29b2727809590a635162a107cee820d5d87f8
Daniel Caballero <danieledgar.caballerohu@elorrieta-errekamari.com>
2025-04-29 11:40:11 +0200

    Creacion de los prototipos de vistas del panel de login y de menu

--- app/src/main/java/com/example/localbooks/MenuActivity.kt
+++ app/src/main/java/com/example/localbooks/MenuActivity.kt
@@ -3,20 +3,19 @@ package com.example.localbooks
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
+        setContentView(R.layout.menu)
+        val button = findViewById<ImageButton>(R.id.imageButton)
         button.setOnClickListener {
 
-            val intent = Intent(this, MenuActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
